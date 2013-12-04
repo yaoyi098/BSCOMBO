57e53805c66d827a57bf1e2602f1f0f30a2b6832
Yao Yimeng <yym900902@gmail.com>
2013-12-04 17:58:00 +0800

    Change Resouce
    ADD HOTKEY control and COMBOLIST control to extend function.

--- resource.h
+++ resource.h
@@ -13,6 +13,8 @@
 #define IDC_RADIO_F                     1002
 #define IDC_RADIO2                      1003
 #define IDC_RADIO_LB                    1003
+#define IDC_COMBO1                      1004
+#define IDC_HOTKEY1                     1005
 
 // Next default values for new objects
 // 
@@ -20,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
